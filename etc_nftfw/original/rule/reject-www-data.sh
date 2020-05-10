694400687ccc3d779bcef2a60ee2d76765bf9f11
Peter Collinson <11645080+pcollinson@users.noreply.github.com>
2020-05-10 23:19:01 +0100

    v0.4.1

--- etc_nftfw/original/rule/reject-www-data.sh
+++ etc_nftfw/original/rule/reject-www-data.sh
@@ -3,6 +3,7 @@
 # Designed for outbound use, prevent packets from
 # the webserver which is user "www-data" from sending anything other than domain requests.
 # Unless the action file contains nominated IP addresses in its list
+# Retained for compatibility - but no recommended for use
 #
 if [ "$DIRECTION" = 'incoming' ]; then
     exit 0
@@ -12,7 +13,7 @@ table $PROTO filter {
       chain reject-www-data {
       	    tcp dport 53 $COUNTER $LOGGER accept
       	    udp dport 53 $COUNTER $LOGGER accept
-      }    
+      }
 }
 EOF
 # white list any IPs
