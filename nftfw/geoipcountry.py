c059f6001874f8ff2b2aee05bcb6586084ed1ad6
Peter Collinson <11645080+pcollinson@users.noreply.github.com>
2020-10-08 10:04:16 +0100

    v0.7.8

--- nftfw/geoipcountry.py
+++ nftfw/geoipcountry.py
@@ -16,13 +16,26 @@ class GeoIPCountry:
     # Country database
     country = '/var/lib/GeoIP/GeoLite2-Country.mmdb'
 
+    # Errors
+    # pylint: disable=invalid-name
+    AddressNotFoundError = None
+    InvalidDatabaseError = None
+
     def __init__(self):
         """Check geoip2 availability
 
         See if the country database file can be found
         """
+
+        # All this is to allow the system to run when geoip2 is not installed
+        # so we don't insist on it
         try:
             from geoip2.database import Reader
+            from geoip2.errors import AddressNotFoundError
+            from maxminddb import InvalidDatabaseError
+            self.AddressNotFoundError = AddressNotFoundError
+            self.InvalidDatabaseError = InvalidDatabaseError
+
             if os.path.exists(self.country):
                 self.countryreader = Reader(self.country)
         except ImportError:
@@ -72,5 +85,5 @@ class GeoIPCountry:
             if cn.country.name:
                 cname = cn.country.name
             return(cname, iso)
-        except (ValueError, AttributeError):
+        except (ValueError, AttributeError, self.AddressNotFoundError, self.InvalidDatabaseError):
             return(None, None)
